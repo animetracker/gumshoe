b05287884e7dac8e621c3e5c56be1e8a153d5689
alexzamurca <alexzamurca@yahoo.co.uk>
2020-08-05 23:47:34 +0100

    Using Navigation Component for bottom navigation view.

--- app/src/main/java/com/alexzamurca/animetrackersprint2/MainActivity.java
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/MainActivity.java
@@ -1,10 +1,8 @@
 package com.alexzamurca.animetrackersprint2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -20,32 +18,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-
-        //By default the list fragment is shown
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ListFragment(), "ListFragment").commit();
+        NavController navController = Navigation.findNavController(this, R.id.fragment_container);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.listFragment, R.id.profileFragment, R.id.settingsFragment).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
-        Fragment selectedFragment = null;
-
-        switch(item.getItemId())
-        {
-            case R.id.navigation_list:
-                selectedFragment = new ListFragment();
-                break;
-
-            case R.id.navigation_profile:
-                selectedFragment = new ProfileFragment();
-                break;
-
-            case R.id.navigation_settings:
-                selectedFragment = new SettingsFragment();
-                break;
-        }
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment, selectedFragment.getClass().getName()).addToBackStack("ListFragment").commit();
-
-        return true;
-    };
 }
