cd448a9c9b33324cbf146c682c438408e9809b71
alexzamurca <alexzamurca@yahoo.co.uk>
2020-08-12 00:03:14 +0100

    The no internet connection now shows even if you have wifi on, but aren't online.

--- app/src/main/java/com/alexzamurca/animetrackersprint2/series/dialog/CheckConnection.java
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/series/dialog/CheckConnection.java
@@ -3,19 +3,44 @@ package com.alexzamurca.animetrackersprint2.series.dialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class CheckConnection
 {
+    private static final String TAG = "CheckConnection";
     private Context context;
 
     public CheckConnection(Context context) {
         this.context = context;
     }
 
-    public boolean isConnected()
+    private boolean hasNetworkConnection()
     {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnected();
     }
+
+    private boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        }
+        catch (IOException | InterruptedException e)
+        {
+            Log.d(TAG, "isOnline: " + e.toString());
+        }
+
+        return false;
+    }
+
+    public boolean isConnected()
+    {
+        return isOnline() && hasNetworkConnection();
+    }
+
 }
