4dbd94216ce38b035a5c1ccb9ce12bca7c69ff83
Jivraj18 <jivraj18@hotmail.com>
2020-08-30 16:04:20 +0100

    ViewPager working, need to add dots and buttons but it swipes properly.

--- app/src/main/java/com/alexzamurca/animetrackersprint2/SliderAdapter.java
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/SliderAdapter.java
@@ -18,15 +18,23 @@ public class SliderAdapter extends PagerAdapter {
 
     // insert screenshots here
     public int[] slide_images = {
-            R.drawable.button,
-            R.drawable.crunchyroll,
-            R.drawable.blue_button
+            R.drawable.screenshot1,
+            R.drawable.screenshot2,
+            R.drawable.screenshot3,
+            R.drawable.screenshot4,
+            R.drawable.screenshot5,
+            R.drawable.screenshot6,
+            R.drawable.screenshot6
     };
 
     public String[] slide_text = {
-            "TEST 1",
-            "TEST 2",
-            "TEST 3"
+            "By pressing the blue plus button, you can see when YOUR favourite episodes release! You can easily add any airing anime to your own custom list to see when they’ll release their next episode!",
+            "Once you press the plus button, you can search for any anime you want! You can add this anime to your list by clicking on it, or you can view additional information by pressing the arrow.",
+            "Includes the thumbnail, next episode date and more! Your friend tell you about a “good” anime? Find out what it’s really about with a glance! You’ll even see ratings by critics and users so you can make the best choice!",
+            "Once you have added your favourite animes to your list, you can do several actions with them. By pressing the first button, you can turn off notifications for that anime. The second button allows you to remove that anime from your list. The third button allows you to choose when you want to be notified for when the next episode is released. The last button allows you to tell us if the release date or time is incorrect.",
+            "Overly obsessive with order? Sort your animes in any and EVERY way that YOU want by pressing the sort button in the top right! We even have a favourites bar for those which you can’t miss!",
+            "The settings tab gives you a variety of features. Under the customisation tab, you can change to dark mode, change your time zone and alert delay.",
+            "Under the general tab, you can report a bug or view our about page, which gives you the contact details of our developers! If you would like to donate to us, you can also do so here. Furthermore, if you enjoy using our app, then you can share it with your friends and family using the share button in the top right."
     };
 
     public SliderAdapter(Context context) {
--- app/src/main/java/com/alexzamurca/animetrackersprint2/TutorialActivity.java
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/TutorialActivity.java
@@ -1,6 +1,8 @@
 package com.alexzamurca.animetrackersprint2;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,20 +12,59 @@ import androidx.viewpager.widget.ViewPager;
 public class TutorialActivity extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
-    private LinearLayout mDotLayout;
+    //private LinearLayout mDotLayout;
 
     private SliderAdapter sliderAdapter;
+
+    //private TextView[] mDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
-        mDotLayout = (LinearLayout) findViewById(R.id.dotLayout);
+        //mDotLayout = (LinearLayout) findViewById(R.id.dotLayout);
         mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
 
         sliderAdapter = new SliderAdapter(this);
 
         mSlideViewPager.setAdapter(sliderAdapter);
+
+        //addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    /*
+    public void addDotsIndicator(int k) {
+
+        mDots = new TextView[7];
+        mDotLayout.removeAllViews();
+
+        for(int  i = 0; i < mDots.length; i++) {
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0) {
+            mDots[k].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+*/
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+           // addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
 }
