61725d47b5b2a4ae35b43e15bcb26f587f296ce4
alexzamurca <alexzamurca@yahoo.co.uk>
2020-09-08 11:43:31 +0100

    Cancel all alarms in list and set all alarms in list now have classes that implement the needed functionality.

---
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/algorithms/SetAlarmsForList.java
@@ -0,0 +1,79 @@
+package com.alexzamurca.animetrackersprint2.algorithms;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+
+import com.alexzamurca.animetrackersprint2.notifications.NotificationAiringChannel;
+import com.alexzamurca.animetrackersprint2.series.Database.SelectTable;
+import com.alexzamurca.animetrackersprint2.series.series_list.Series;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class SetAlarmsForList
+{
+    private Context context;
+
+    public SetAlarmsForList(Context context) {
+        this.context = context;
+    }
+
+    public void run()
+    {
+        getTableAndSetAlarms();
+    }
+
+    private void getTableAndSetAlarms()
+    {
+        GetTableAsync getTableAsync = new GetTableAsync();
+        getTableAsync.execute();
+    }
+
+    private void setAllAlarms(List<Series> currentList)
+    {
+        NotificationAiringChannel notificationAiringChannel = new NotificationAiringChannel(context);
+        for(int i = 0; i < currentList.size(); i++)
+        {
+            Series selectedSeries = currentList.get(i);
+
+            AdjustAirDate adjustAirDate = new AdjustAirDate(selectedSeries);
+            Calendar calendar = adjustAirDate.getCalendar();
+
+            if(calendar!=null)
+            {
+                notificationAiringChannel.setNotification(selectedSeries, calendar);
+            }
+        }
+    }
+
+    private class GetTableAsync extends AsyncTask<Void, Void, Void>
+    {
+        private boolean wasRequestSuccessful;
+        private ArrayList<Series> list;
+
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+            SharedPreferences sharedPreferences = context.getSharedPreferences("Account", Context.MODE_PRIVATE);
+            String session = sharedPreferences.getString("session", "");
+            SelectTable selectTable = new SelectTable(session);
+            list = selectTable.getSeriesList();
+            wasRequestSuccessful = selectTable.getWasRequestSuccessful();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            if(wasRequestSuccessful)
+            {
+                setAllAlarms(list);
+            }
+
+            super.onPostExecute(aVoid);
+        }
+    }
+}
