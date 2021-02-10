86348cad518783c9bee3af22883ead140eeb966c
alexzamurca <alexzamurca@yahoo.co.uk>
2021-02-10 17:10:46 +0000

    programmed to change button on icon state, buttons trigger a dialog. Dialog works mostly except for changing the profile icon in the settings and list toolbar.

---
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/dialog/ProfileIconChangeConfirmationDialog.java
@@ -0,0 +1,117 @@
+package com.alexzamurca.animetrackersprint2.dialog;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.alexzamurca.animetrackersprint2.R;
+
+public class ProfileIconChangeConfirmationDialog  extends DialogFragment
+{
+    private static final String TAG = "ProfileIconChangeConfirmationDialog";
+
+    String[] cardNames = new String[]{"Default", "Ash", "Goku", "Naruto", "Luffy", "Eren"};
+    int[] cardPrices = new int[]{0, 500, 500, 750, 1000, 1000};
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_profile_icon_change_confirmation, container, false);
+        setCancelable(false);
+
+        Window window = requireDialog().getWindow();
+        if(window!=null)
+        {
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+        assert getArguments() != null;
+        int index  = getArguments().getInt("index");
+        int state  = getArguments().getInt("state");
+
+        TextView explanation = view.findViewById(R.id.profile_icon_change_confirmation_explanation);
+        String question = "";
+        if(state == 1)
+        {
+            question = "Do you want to use the " + cardNames[index] + " Profile Icon?";
+        }
+        else if(state == 0)
+        {
+            question = "Do you want to spend "+  Integer.toString(cardPrices[index]) + " Gumshoe Points to purchase the "+ cardNames[index] + " Profile Icon?";
+        }
+        explanation.setText(question);
+
+        Button yesButton = view.findViewById(R.id.profile_icon_change_confirmation_yes_button);
+        yesButton.setOnClickListener(v ->
+        {
+            yesLogic(index, state);
+            dismiss();
+        });
+
+        Button noButton = view.findViewById(R.id.profile_icon_change_confirmation_no_button);
+        noButton.setOnClickListener(v -> dismiss());
+
+        ImageButton closeButton = view.findViewById(R.id.profile_icon_change_confirmation_close);
+        closeButton.setOnClickListener(v -> dismiss());
+        return view;
+    }
+
+    private void yesLogic(int index, int state)
+    {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("Profile Icons", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        // if buying the icon
+        if (state == 0)
+        {
+            // taking money away
+            int points = sharedPreferences.getInt("points", 0);
+            if(points >= cardPrices[index])
+            {
+                // changing the shared preferences of that index from 0 to 1
+                editor.putInt(cardNames[index], 1);
+                editor.putInt("points", points - cardPrices[index]);
+
+                editor.apply();
+            }
+            else
+            {
+                Log.d(TAG, "yesLogic: not enought money to get this icon");
+            }
+        }
+        // if selecting the icon
+        else if (state == 1)
+        {
+            // for loop through shared preferences of icons, if in state 0,1, do nothing, if in state 2 then change to state 1
+            for(int i = 0; i < cardNames.length; i++)
+            {
+                int temp_state = sharedPreferences.getInt(cardNames[i], 0);
+                if(temp_state == 2)
+                {
+                    editor.putInt(cardNames[i], 1);
+                    editor.apply();
+                }
+            }
+            // change shared preferences for that index from 1 to 2
+            editor.putInt(cardNames[index], 2);
+            editor.apply();
+
+            // change profile icon logic
+        }
+
+        // update list
+    }
+}
---
+++ app/src/main/java/com/alexzamurca/animetrackersprint2/settings/ChangeProfileIconFragment.java
@@ -0,0 +1,130 @@
+package com.alexzamurca.animetrackersprint2.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.alexzamurca.animetrackersprint2.R;
+import com.alexzamurca.animetrackersprint2.dialog.NotificationsOffDialog;
+import com.alexzamurca.animetrackersprint2.dialog.ProfileIconChangeConfirmationDialog;
+import com.google.android.material.snackbar.Snackbar;
+
+public class ChangeProfileIconFragment extends Fragment
+{
+    private Button[] cardButtons;
+    private transient FragmentActivity mContext;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_change_profile_icon, container, false);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context)
+    {
+        mContext = (FragmentActivity)context;
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        cardButtons = new Button[]{view.findViewById(R.id.default_button), view.findViewById(R.id.ash_button), view.findViewById(R.id.goku_button), view.findViewById(R.id.naruto_button), view.findViewById(R.id.luffy_button), view.findViewById(R.id.eren_button)};
+
+        Toolbar toolbar = view.findViewById(R.id.change_profile_toolbar_object);
+        AppCompatActivity activity = (AppCompatActivity) requireActivity();
+        activity.setSupportActionBar(toolbar);
+        if(activity.getSupportActionBar()!=null)
+        {
+            activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        // 0 = default, 1 = Ash (Pokemon), 2 = Goku (Dragon Ball),  3 = Naruto, 4 = Luffy (One Piece), 5 = Eren (Attack On Titan)
+        initCardView();
+        manageOnClicks(view);
+    }
+
+    private void initCardView()
+    {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("Profile Icons", Context.MODE_PRIVATE);
+        String[] sharedPreferencesKeys = new String[]{"Default", "Ash", "Goku", "Naruto", "Luffy", "Eren"};
+        String[] cardPrices = new String[]{"0 Points", "500 Points", "500 Points", "750 Points", "1000 Points", "1000 Points"};
+        for (int i = 0; i < sharedPreferencesKeys.length; i++)
+        {
+            String key = sharedPreferencesKeys[i];
+            int defaultValue = 0;
+            if(i==0) defaultValue = 2;
+            int state = sharedPreferences.getInt(key, defaultValue);
+            // Alter button according to state
+            Button button = cardButtons[i];
+            switch(state)
+            {
+                case(0):button.setVisibility(View.VISIBLE);
+                    button.setText(cardPrices[i]);
+                    button.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.blue_with_rounded_corners));
+                    break;
+
+                case(1):
+                    button.setVisibility(View.VISIBLE);
+                    button.setText("Use");
+                    button.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.red_with_rounded_corners));
+                    break;
+
+                case(2):
+                    button.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void manageOnClicks(View view)
+    {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("Profile Icons", Context.MODE_PRIVATE);
+        String[] sharedPreferencesKeys = new String[]{"Default", "Ash", "Goku", "Naruto", "Luffy", "Eren"};
+        int[] cardPrices = new int[]{0, 500, 500, 750, 1000, 1000};
+
+        for(int j = 0; j < cardButtons.length; j++)
+        {
+            Button button = cardButtons[j];
+            int finalJ = j;
+            int defaultValue = 0;
+            if(finalJ ==0) defaultValue = 2;
+            int state = sharedPreferences.getInt(sharedPreferencesKeys[finalJ], defaultValue);
+            int points = sharedPreferences.getInt("points", 0);
+            button.setOnClickListener(v ->
+            {
+                // if buying and have money or in state 1
+                if(((state == 0) && (points >= cardPrices[finalJ])) || (state == 1))
+                {
+
+                    ProfileIconChangeConfirmationDialog dialog = new ProfileIconChangeConfirmationDialog();
+                    Bundle args = new Bundle();
+                    args.putInt("index", finalJ);
+
+                    args.putInt("state", state);
+                    dialog.setArguments(args);
+                    dialog.show(mContext.getSupportFragmentManager(), "ProfileIconChangeConfirmationDialog");
+                }
+                else if(points < cardPrices[finalJ])
+                {
+                    Snackbar.make(view, "You do not have enough points to buy this icon!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+            );
+        }
+    }
+}
